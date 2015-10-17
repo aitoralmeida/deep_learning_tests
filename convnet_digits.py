089dacdafea0df3fb74f5148eb345458aa5ddcc5
Aitor <aitoralmeida@gmail.com>
2015-10-17 17:03:37 +0200

    added network training

--- convnet_digits.py
+++ convnet_digits.py
@@ -60,6 +60,62 @@ def load_dataset():
 
 print 'Loading dataset...'    
 X_train, y_train, X_val, y_val, X_test, y_test = load_dataset()
+
 print 'Showing img...'
 plt.imshow(X_train[0][0], cmap=cm.binary)
+
+print 'Training the network:'
+print '  -Building the net...'
+net1 = NeuralNet(
+    layers=[('input', layers.InputLayer),
+            ('conv2d1', layers.Conv2DLayer),
+            ('maxpool1', layers.MaxPool2DLayer),
+            ('conv2d2', layers.Conv2DLayer),
+            ('maxpool2', layers.MaxPool2DLayer),
+            ('dropout1', layers.DropoutLayer),
+            ('dense', layers.DenseLayer),
+            ('dropout2', layers.DropoutLayer),
+            ('output', layers.DenseLayer),
+            ],
+    # input layer
+    input_shape=(None, 1, 28, 28),
+    # layer conv2d1
+    conv2d1_num_filters=32,
+    conv2d1_filter_size=(5, 5),
+    conv2d1_nonlinearity=lasagne.nonlinearities.rectify,
+    conv2d1_W=lasagne.init.GlorotUniform(),  
+    # layer maxpool1
+    maxpool1_pool_size=(2, 2),    
+    # layer conv2d2
+    conv2d2_num_filters=32,
+    conv2d2_filter_size=(5, 5),
+    conv2d2_nonlinearity=lasagne.nonlinearities.rectify,
+    # layer maxpool2
+    maxpool2_pool_size=(2, 2),
+    # dropout1
+    dropout1_p=0.5,    
+    # dense
+    dense_num_units=256,
+    dense_nonlinearity=lasagne.nonlinearities.rectify,    
+    # dropout2
+    dropout2_p=0.5,    
+    # output
+    output_nonlinearity=lasagne.nonlinearities.softmax,
+    output_num_units=10,
+    # optimization method params
+    update=nesterov_momentum,
+    update_learning_rate=0.01,
+    update_momentum=0.9,
+    max_epochs=10,
+    verbose=1,
+    )
+print '  -Training the net...'
+# Train the network
+nn = net1.fit(X_train, y_train)
+
+
+
+
 print 'FIN'
+
+
